779b7176c5bc521038101fdf97fb8cae2db8960c
CeliaFernandez <celia.fernandez.madrazo@cern.ch>
2022-09-15 17:03:36 +0200

    Add signal .dat files with Summer22 NTuplizer version, add MET ntuplas, update the file to create dat files

--- createDatFile.py
+++ createDatFile.py
@@ -9,12 +9,12 @@ color_dict['10000'] = 'r.kMagenta+1'
 
 space = '      '
 
-eosdir = '/eos/cms/store/user/pablom/ntuplesForChus/2018/'
+eosdir = '/eos/user/f/fernance/Galapago/June2022/'
 prefix = 'ggH_HToSSTo4l'
-#outdir = 'NTuples-Galapago-2017'
+outdir = 'NTuples-Galapago_2018' # adapt year here!
 ndir   = '0000'
 
-outdat = 'signals_2018UL.dat'
+outdat = 'signals_2018UL_Summer22.dat' # adapt year here!
 outdat_ = open(outdat, 'w')
 
 ## loop over samples:
@@ -22,13 +22,15 @@ outdat_ = open(outdat, 'w')
 for _d in os.listdir(eosdir):
     sample_dir = eosdir
     if _d[:13] != prefix: continue
-    sample_dir += _d + ''
-    #sample_dir += outdir + '/'
+    sample_dir += _d + '/'
+    sample_dir += outdir + '/'
+
     mH   = (sample_dir.split('MH-')[-1]).split('_')[0]
     mS   = (sample_dir.split('MS-')[-1]).split('_')[0]
     ctau = (sample_dir.split('ctauS-')[-1]).split('_')[0]
-    sample_name = 'HSS_' + mH + '_' + mS + '_' + ctau + '_2018'
+    sample_name = 'HSS_' + mH + '_' + mS + '_' + ctau + '_2018' # adapt year here!
     sample_label = 'HSS({0},{1},{2})'.format(mH, mS, ctau)
+
     if os.path.isdir(sample_dir):
         sample_dir += os.listdir(sample_dir)[-1] + '/0000/'
         line = sample_name 
